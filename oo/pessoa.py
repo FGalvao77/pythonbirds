cdfd580209b8c6868c7db842196dbb4c0ba499c3
fgalvao77 <fernando.galvao77@outlook.com>
2020-04-08 20:15:43 -0300

    Criados atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,11 +1,22 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=43):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Cesar')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Fernando'
+    print(p.nome)
+    print(p.idade)
+
+
+
 
